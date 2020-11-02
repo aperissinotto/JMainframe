c664e1255404e1d0a5657b985cd8754f848ff540
aperissinotto <alexandrepamaral@gmail.com>
2020-11-01 21:54:09 -0300

    novo

--- src/perissinotto/com/it/model/Mensagem.java
+++ src/perissinotto/com/it/model/Mensagem.java
@@ -6,19 +6,20 @@ public class Mensagem {
 	private byte[] mensagem;
 	private int tamanhoMensagem;
 	
-	private char[] hexChars = new char[tamanhoMensagem * 2];
-	
 	public Mensagem(byte[] mensagem, int tamanhoMensagem) {
 		this.mensagem = mensagem;
 		this.tamanhoMensagem = tamanhoMensagem;
 	}
 
 	public String paraHex() {
+		char[] hexChars = new char[this.tamanhoMensagem * 2];
+		
 		for (int j = 0; j < this.tamanhoMensagem; j++) {
 			int v = this.mensagem[j] & 0xFF;
 			hexChars[j * 2] = HEX_ARRAY[v >>> 4];
 			hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
 		}
+		
 		return String.copyValueOf(hexChars);
 	}
 }
